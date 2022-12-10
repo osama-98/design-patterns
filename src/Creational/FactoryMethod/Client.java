bc9700a7c5c3e222763be651c984c87610b9080f
osama saadah <osama.sada98@gmail.com>
2022-12-11 00:39:14 +0300

    creators

--- src/Creational/FactoryMethod/Client.java
+++ src/Creational/FactoryMethod/Client.java
@@ -2,16 +2,18 @@ package Creational.FactoryMethod;
 
 import Creational.FactoryMethod.Creators.JSONMessageCreator;
 import Creational.FactoryMethod.Creators.MessageCreator;
+import Creational.FactoryMethod.Creators.TextMessageCreator;
 import Creational.FactoryMethod.Message.Message;
 
 public class Client {
 
 	public static void main(String[] args) {
 		printMessage(new JSONMessageCreator());
+		printMessage(new TextMessageCreator());
 	}
 	
 	public static void printMessage(MessageCreator creator) {
 		Message message = creator.getMessage();
-		System.out.println(message);
+		System.out.println(message.getContent());
 	}
 }
